33210d7add3d510099e59e52266f3999bf6f83d9
Ramón Doval Dev <ramondoval79@gmail.com>
2026-03-05 20:34:17 +0100

    Main setup, install NextJS and ShadCN, and build root layout with its components

---
+++ components/UserDropdown.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Button } from "./ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { LogOut } from "lucide-react";
+import NavItems from "./NavItems";
+
+const UserDropdown = () => {
+  const router = useRouter();
+  const handleSignOut = () => {
+    router.push("/");
+  };
+  const user = { name: "Ramón Doval", email: "ramon@ramon.com" };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant={"ghost"}
+          className="flex items-center gap-3 text-gray-400 hover:text-yellow-500"
+        >
+          <Avatar className="w-8 h-8">
+            <AvatarImage
+              src={
+                "https://yt3.ggpht.com/yti/ANjgQV8FUW0X4cF0aEnOtc33hkAkyEHRJIaIUcGpY1iLs8U=s88-c-k-c0x00ffffff-no-rj"
+              }
+            />
+            <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
+              {user.name[0]}
+            </AvatarFallback>
+            <div className="hidden md:flex flex-col items-start">
+              <span className="text-base text-gray-400 font-medium">
+                {user.name}
+              </span>
+            </div>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="text-gray-400">
+        <DropdownMenuLabel>
+          <div className="flex relative items-center gap-3 py-2">
+            <Avatar className="h-10 w-10">
+              <AvatarImage
+                src={
+                  "https://yt3.ggpht.com/yti/ANjgQV8FUW0X4cF0aEnOtc33hkAkyEHRJIaIUcGpY1iLs8U=s88-c-k-c0x00ffffff-no-rj"
+                }
+              />
+              <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
+                {user.name[0]}
+              </AvatarFallback>
+            </Avatar>
+            <div className="flex flex-col">
+              <span className="text-base font-medium text-gray-400">
+                {user.name}
+              </span>
+              <span className="text-sm text-gray-500">{user.email}</span>
+            </div>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator className="bg-gray-600" />
+        <DropdownMenuItem
+          onClick={handleSignOut}
+          className="text-gray-100 text-md font-medium focus:bg-transparent focus:text-yellow-500 transition-colors cursor-pointer"
+        >
+          <LogOut className="h-4 w-4 mr-2 hidden sm:block" />
+          Logout
+        </DropdownMenuItem>
+        <DropdownMenuSeparator className="hidden sm:block bg-gray-600" />
+        <nav className="sm:hidden">
+          <NavItems />
+        </nav>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default UserDropdown;
